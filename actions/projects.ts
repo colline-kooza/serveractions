27a788c05e24a23197d25cdac139ffd72f42c89f
kooza-colline <koozacollinz1@gmail.com>
2024-05-06 17:31:26 +0300

    initial changes

--- actions/projects.ts
+++ actions/projects.ts
@@ -1,11 +1,7 @@
 "use server"
-
 import { ProjectProps } from "@/types/types";
 import { PrismaClient } from "@prisma/client";
 import { revalidatePath } from "next/cache";
-
-
-
 const prisma = new PrismaClient()
 
 export async function saveData(data:ProjectProps){
@@ -20,6 +16,7 @@ export async function saveData(data:ProjectProps){
        console.log(error) 
     }
 }
+
 export async function getProjects(){
     try {
       const projects = await prisma.project.findMany()  
